4f5643509e331b76b46bc16abc3d76608849b837
De-voice <vsignal751@gmail.com>
2021-04-20 17:15:32 +0100

    david

--- src/Container/ArtistsList.js
+++ src/Container/ArtistsList.js
@@ -3,7 +3,7 @@ import React from 'react'
 function ArtistsList() {
     return (
         <div className="bg-gray-400 rounded-full w-1/6 h-4/6">
-          fnf  
+           
         </div>
     )
 }
